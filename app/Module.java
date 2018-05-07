9804df691b1db29e4f511723f0401d46b5b52d9f
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-05-07 14:36:02 +0200

    FLUX-133 Removed unused code

--- app/Module.java
+++ app/Module.java
@@ -1,22 +1,10 @@
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoClientURI;
-import com.typesafe.config.Config;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import repositories.measurements.MeasurementsRepository;
 import repositories.measurements.MeasurementsRepositoryJPA;
 import repositories.projects.ProjectsRepository;
 import repositories.projects.ProjectsRepositoryJPA;
 import repositories.rooms.RoomsRepository;
 import repositories.rooms.RoomsRepositoryJPA;
-
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -37,17 +25,5 @@ public class Module extends AbstractModule {
         bind(RoomsRepository.class).to(RoomsRepositoryJPA.class);
         bind(MeasurementsRepository.class).to(MeasurementsRepositoryJPA.class);
         bind(ProjectsRepository.class).to(ProjectsRepositoryJPA.class);
-    }
-
-    @Provides @Singleton @Inject
-    MongoClient provideMongoClient(final Config config) {
-        final CodecRegistry codecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
-        final MongoClientOptions.Builder mongoClientOptions = MongoClientOptions.builder()
-                .codecRegistry(codecRegistry);
-        final String uri = config.getString("flux.mongodb.uri");
-        final MongoClientURI mongoUri = new MongoClientURI(uri, mongoClientOptions);
-        return new MongoClient(mongoUri);
     }
 }
