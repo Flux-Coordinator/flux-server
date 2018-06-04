7ec688a82e5b20c15c1976c9e2c0fab058609052
Patrick Scherler <pacs01dev@gmail.com>
2018-06-04 19:31:24 +0200

    add filter to observe sensor activity

---
+++ app/filters/SensorActivityFilter.java
@@ -0,0 +1,29 @@
+package filters;
+
+import akka.stream.Materializer;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import javax.inject.Inject;
+import play.mvc.Filter;
+import play.mvc.Http.RequestHeader;
+import play.mvc.Result;
+
+public class SensorActivityFilter extends Filter {
+
+    private static final String SENSOR_DEVICE_HEADER = "X-Flux-Sensor";
+    public static long lastActivity = 0;
+
+    @Inject
+    public SensorActivityFilter(Materializer mat) {
+        super(mat);
+    }
+
+    @Override
+    public CompletionStage<Result> apply(Function<RequestHeader, CompletionStage<Result>> nextFilter,
+        RequestHeader requestHeader) {
+        if (requestHeader.hasHeader(SENSOR_DEVICE_HEADER)){
+            lastActivity = System.currentTimeMillis();
+        }
+        return nextFilter.apply(requestHeader);
+    }
+}
