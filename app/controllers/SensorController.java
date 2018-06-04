030c36d2ae0ccdd88a858ed893e00c0793b62a59
Patrick Scherler <pacs01dev@gmail.com>
2018-06-04 19:31:45 +0200

    implement sensor controller

--- app/controllers/SensorController.java
+++ app/controllers/SensorController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import authentication.JWTAuthenticator;
+import filters.SensorActivityFilter;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -8,8 +9,16 @@ import play.mvc.Security;
 @Security.Authenticated(value = JWTAuthenticator.class)
 public class SensorController extends Controller {
 
+    private static final long SENSOR_DEVICE_TIMEOUT = 10000;
+
     public Result getSensorState() {
-        return ok();
+        if (SensorActivityFilter.lastActivity == 0) {
+            return notFound();
+        } else if (System.currentTimeMillis() - SensorActivityFilter.lastActivity < SENSOR_DEVICE_TIMEOUT) {
+            return ok();
+        } else {
+            return noContent();
+        }
     }
 
 }
