a4b7551d12d42a348167ead437a2d12b5a6244ed
Patrick Scherler <pacs01dev@gmail.com>
2018-06-05 09:39:36 +0200

    use Date type for last sensor activity

--- app/controllers/SensorController.java
+++ app/controllers/SensorController.java
@@ -5,6 +5,7 @@ import filters.SensorActivityFilter;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import java.util.Date;
 
 @Security.Authenticated(value = JWTAuthenticator.class)
 public class SensorController extends Controller {
@@ -12,7 +13,8 @@ public class SensorController extends Controller {
     private static final long SENSOR_DEVICE_TIMEOUT = 10000;
 
     public Result getSensorState() {
-        if (SensorActivityFilter.lastActivity != 0 && System.currentTimeMillis() - SensorActivityFilter.lastActivity < SENSOR_DEVICE_TIMEOUT) {
+        Date currentDate = new Date();
+        if (SensorActivityFilter.lastActivity != null && currentDate.getTime() - SensorActivityFilter.lastActivity.getTime() < SENSOR_DEVICE_TIMEOUT) {
             return ok("Sensor is active");
         } else {
             return notFound("No sensor connected");
--- app/filters/SensorActivityFilter.java
+++ app/filters/SensorActivityFilter.java
@@ -3,6 +3,7 @@ package filters;
 import akka.stream.Materializer;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
+import java.util.Date;
 import javax.inject.Inject;
 import play.mvc.Filter;
 import play.mvc.Http.RequestHeader;
@@ -11,7 +12,7 @@ import play.mvc.Result;
 public class SensorActivityFilter extends Filter {
 
     private static final String SENSOR_DEVICE_HEADER = "X-Flux-Sensor";
-    public static long lastActivity = 0;
+    public static Date lastActivity = null;
 
     @Inject
     public SensorActivityFilter(Materializer mat) {
@@ -21,8 +22,8 @@ public class SensorActivityFilter extends Filter {
     @Override
     public CompletionStage<Result> apply(Function<RequestHeader, CompletionStage<Result>> nextFilter,
         RequestHeader requestHeader) {
-        if (requestHeader.hasHeader(SENSOR_DEVICE_HEADER)){
-            lastActivity = System.currentTimeMillis();
+        if (requestHeader.hasHeader(SENSOR_DEVICE_HEADER)) {
+            lastActivity = new Date();
         }
         return nextFilter.apply(requestHeader);
     }
